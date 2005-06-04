b7fe14188be5cdee5d6541d8e9e6c583d298d858
vasimos <vasimos@zero.physics.gatech.edu>
2005-06-04 15:25:26 +0000

    Modified oscillator.f90: time is the last coordinate now. Modified rpoinc.nb to display only Poincare section and return map (not trajectory).

--- programs/testing/SHO/trunk/oscillator.f90
+++ programs/testing/SHO/trunk/oscillator.f90
@@ -8,9 +8,9 @@ REAL(dp), INTENT(IN) :: x
 REAL(dp), DIMENSION(:), INTENT(IN):: y
 REAL(dp), DIMENSION(:), INTENT(OUT) :: dydx
 
-dydx(1)=1
+
 dydx(2)=y(3)
 dydx(3)=-y(2)
-
+dydx(1)=1
 
 END SUBROUTINE
