3e6b3fd8d7e28b278d8fbc86b367ac84acaf3c58
vasimos <vasimos@zero.physics.gatech.edu>
2005-06-18 02:36:53 +0000

    Corrected signs in KSfield. Program overflows after some time.

--- programs/testing/KSint/trunk/integr.f90
+++ programs/testing/KSint/trunk/integr.f90
@@ -1,4 +1,3 @@
-! Exercise 2.6
 
 ! Fourth order Runge-Kutta integrator. Needs the module nrtype from Numerical Recipes,
 ! that defines data types. Time is treated as an extra variable of the system 
@@ -8,41 +7,51 @@
 
 PROGRAM integr
 
-
-USE nrtype; USE ifc_integr
+use parameters
+USE nrtype
+USE ifc_integr
 
 IMPLICIT NONE
 
 REAL(dp), DIMENSION(:,:), ALLOCATABLE :: y
-REAL(dp) :: xi=0, xf=10
-INTEGER(I4B) :: nsteps=200, dmn=3,i
+REAL(dp) :: xi=0.0_dp, xf=10000.0_dp
+INTEGER(I4B) :: nsteps=1000000, d=16,i
+
 
 INTERFACE
-	SUBROUTINE oscillator(x,y,dydx)
+	SUBROUTINE KSfield(x,y,dydx)
 		USE nrtype
 		IMPLICIT NONE
 		REAL(dp), INTENT(IN) :: x
 		REAL(dp), DIMENSION(:), INTENT(IN) :: y
 		REAL(dp), DIMENSION(:), INTENT(OUT) :: dydx	
-	END SUBROUTINE oscillator
+	END SUBROUTINE KSfield
 END INTERFACE
 
+allocate(y(nsteps+1,2*d+1))
 
+y=0.0_dp
 
-allocate(y(nsteps+1,dmn))
+OPEN(10, file='ic.dat')
 
-y=0
+	read(10,*) y(1,d+1:2*d) 
 
-y(1,1)=xi
-y(1,2)=0
-y(1,3)=1
+CLOSE(10)
 
-call rk4driver(xi,y(1,:),xf,nsteps,y,oscillator) 
+y(1,2*d+1)=xi
 
-OPEN(10, file='oscillator.dat')
+y(1,1)=1.0_dp
+y(1,2)=1.0_dp
+y(1,4)=1.0_dp
+y(1,6)=1.0_dp
+y(1,9)=1.0_dp
+
+call rk4driver(xi,y(1,:),xf,nsteps,y,KSfield) 
+
+OPEN(10, file='ks.dat')
 
 DO i=1, size(y,1)
-	WRITE(10,*) y(i,:) 
+	WRITE(10,*) y(i,d+1:d+3) 
 END DO
 
 CLOSE(10) 
