7653cd10aaeb16ed2821b2f1464cff5e742cca9a
vasimos <vasimos@zero.physics.gatech.edu>
2005-07-18 19:02:21 +0000

    Bugfix: Calculation of r_1,r_2. Skipped intermediate transformation in going from KS-McGehee to Cartesian.

--- programs/threeBody/trunk/KSMcGtoCart_TBL.f90
+++ programs/threeBody/trunk/KSMcGtoCart_TBL.f90
@@ -2,17 +2,20 @@ subroutine KSMcGtoCart_TBL(Et,Q,P,hyperR,x,p_x,E)
 
 use nrtype
 
-real(dp), dimension(:), intent(in) :: Q,P,r
+implicit none
+
+real(dp), dimension(:), intent(in) :: Q,P
 real(dp), intent(in)::  Et, hyperR
 real(dp), dimension(:), intent(out):: x,p_x
 real(dp), intent(out):: E 
 
-implicit none
+real(dp), dimension(size(Q)) :: Qdum,rdum
 
-x(1)=Q(1)**2
-x(2)=-Q(2)**2
-p_x(1)=Q(1)*P(1)
-p_x(2)=-Q(4)*P(4)
+!
+x(1)=hyperR*Q(1)**2
+x(2)=-hyperR*Q(2)**2
+p_x(1)=Q(1)*P(1)/(2*Sqrt(hyperR)*Q(1)**2)
+p_x(2)=-Q(2)*P(2)/(2*Sqrt(hyperR)*Q(2)**2)
 E=Et/hyperR
 
 end subroutine
--- programs/threeBody/trunk/ifc_threeBody.f90
+++ programs/threeBody/trunk/ifc_threeBody.f90
@@ -2,23 +2,13 @@ module ifc_threeBody
 
 
 interface
-	subroutine CartToKSMcG_TBL(x,p_x,dt,E,Q,P,r,dtau,Et,hyperR)
+	subroutine CartToKSMcG_TBL(x,p_x,E,Q,P,r,Et,hyperR)
 		use nrtype
 		implicit none
 		real(dp), dimension(:), intent(in):: x,p_x
-		real(dp), intent(in):: dt, E 
+		real(dp), intent(in):: E 
 		real(dp), dimension(:), intent(out) :: Q,P,r
-		real(dp), intent(out):: dtau, Et, hyperR
-	end subroutine
-end interface
-
-interface
-	subroutine EOM_TBL(Q,P,r,E,hyperR,v)
-		use nrtype
-		implicit none
-		real(dp), dimension(:), intent(in) :: Q,P,r
-		real(dp), intent(in) :: E,hyperR
-		real(dp), dimension(:), intent(out) :: v
+		real(dp), intent(out)::Et, hyperR
 	end subroutine
 end interface
 
@@ -26,7 +16,7 @@ interface
 	subroutine KSMcGtoCart_TBL(Et,Q,P,hyperR,x,p_x,E)
 		use nrtype
 		implicit none
-		real(dp), dimension(:), intent(in) :: Q,P,r
+		real(dp), dimension(:), intent(in) :: Q,P
 		real(dp), intent(in)::  Et, hyperR
 		real(dp), dimension(:), intent(out):: x,p_x
 		real(dp), intent(out):: E 
