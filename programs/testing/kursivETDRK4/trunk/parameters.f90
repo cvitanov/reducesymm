38ed3ff56199026541e6b1ac220b90606e9a27d6
vasimos <vasimos@zero.physics.gatech.edu>
2005-08-24 14:42:47 +0000

    integrates initial condition close to Lan's tori.

--- programs/testing/kursivETDRK4/trunk/parameters.f90
+++ programs/testing/kursivETDRK4/trunk/parameters.f90
@@ -2,10 +2,10 @@ module parameters
 
 use nrtype
 
-real(dp), parameter :: L=6.51739492_dp ! Dimensionless length used here
-integer(i4b), parameter :: M=32, d=256
-real(dp), parameter :: h=0.025_dp, R=1.0_dp, ti=0.0_dp,tf=2000.0_dp
-integer(i4b),parameter :: Nplt=2000
+real(dp), parameter :: L=6.51739491961311490087_dp ! Dimensionless length used here
+integer(i4b), parameter :: M=32, d=32, Niter=30
+real(dp), parameter :: h=0.001_dp, R=1.0_dp, ti=0.0_dp,tf=1000.0_dp
+integer(i4b),parameter :: Nplt=1000000
 character(len=*), parameter :: frm_t='(1000F12.4)', frm_u='(256F19.12)', frm_a='(129F19.12)'
 
 end module parameters
