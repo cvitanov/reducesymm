84b57a8b5eaac85b0cdedf43161c36d324b96e16
vasimos <vasimos@zero.physics.gatech.edu>
2005-09-23 01:09:33 +0000

    Branch roesslerJ into a simple test program for coupled oscillators.

---
+++ programs/testing/coupledOsc/trunk/parameters.f90
@@ -0,0 +1,11 @@
+module parameters
+
+use nrtype
+
+implicit none
+
+real(dp), parameter :: Tguess=5.4_dp , alpha=0.2_dp, beta=0.2_dp, gamma=5.7_dp
+integer(i4b), parameter ::  d=3 
+CHARACTER(len=*), parameter :: format_label='(3F16.10)'
+
+end module
