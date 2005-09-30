efe977865f3113916662e6862c559f8494f4351c
vasimos <vasimos@zero.physics.gatech.edu>
2005-09-30 06:33:17 +0000

    Clean compilation achieved.

--- programs/RPO/newton/trunk/setLHM.f90
+++ programs/RPO/newton/trunk/setLHM.f90
@@ -14,8 +14,8 @@ complex(dpc), dimension(:,:), intent(out) :: LHM
 integer(i4b) :: ndum
 
 ndum = assert_eq(size(v),size(J,1),size(f),'setLHM 1')
-ndum = assert_eq(ndum,q,size(LHM,1)-2,'setLHM 2')
-ndum = assert_eq(ndum,R,diagk,'setLHM 3')
+ndum = assert_eq(ndum,size(q),size(LHM,1)-2,'setLHM 2')
+ndum = assert_eq(ndum,size(R),size(diagk),'setLHM 3')
 ndum = assert_eq(ndum,size(LHM,2)-2,'setLHM 4')
 
 LHM=0.0_dp
@@ -24,11 +24,11 @@ J = DiagMul(R,J,ndum) !! Destroys J, but we only need RJ
 
 LHM(1:size(v),1:size(v)) = UnitMatrix(size(v)) - J 
 
-LHM(size(LHM,1)-1,1:size(q)) = q*J
+LHM(size(LHM,1)-1,1:size(q)) = MatMul(q,J) 
 LHM(size(LHM,1),1:size(q)) = q
 
 LHM(1:size(v),size(LHM,1)-1) = -R*v
-LHM(1:size(v),size(LHM,1)) = -ii*(R*(diagk*f))/L
+LHM(1:size(v),size(LHM,1)) = -R*(diagk*f)
 
 LHM(size(LHM,1)-1,size(LHM,1)-1) = -dot_product(q,LHM(1:size(v),size(LHM,1)-1))
 LHM(size(LHM,1)-1,size(LHM,1)) = -dot_product(q,LHM(1:size(v),size(LHM,1)))
