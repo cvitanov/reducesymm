c627e530dee1ecfe32e9d538aa77c49797344ad6
vasimos <vasimos@zero.physics.gatech.edu>
2005-11-26 22:53:51 +0000

    ap_pattern3: working in Lan's length, full space. Producing many guesses for POs.

--- programs/testing/kursivRPO/branches/ap_pattern3/parameters.f90
+++ programs/testing/kursivRPO/branches/ap_pattern3/parameters.f90
@@ -2,7 +2,7 @@ module parameters
 
 use nrtype
 
-real(dp), parameter :: L=3.063732654518985213439735693086163337284_dp ! Dimensionless length used here
+real(dp), parameter :: L=6.1274653090379711529_dp ! Dimensionless length used here.
 integer(i4b), parameter :: M=32, d=128
 integer(i4b),dimension(2), parameter :: seed=(/1727,1234/)
 real(dp), parameter :: h=0.025_dp, R=1.0_dp, ti=0.0_dp,tf=10000.0_dp
