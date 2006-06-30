cedd9bfc99f8d53513da5f5c8972b2b4116edda7
siminos <siminos@zero.physics.gatech.edu>
2006-06-30 02:20:42 +0000

    Create a folder for the results for KSe, Full Space, L=22.0.

---
+++ programs/production/KS22.0/equil/trunk/parameters.f90
@@ -0,0 +1,10 @@
+module parameters
+
+use nrtype
+
+integer(i4b), parameter :: ntrial=1000
+integer(i4b), parameter :: d = 128
+real(dp), parameter:: L=6.1274653090379711529_dp !Dimensionless length used here
+real(dp), parameter :: tolbc=1d-13, tolf=5d-10
+
+end module
