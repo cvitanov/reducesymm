314dc76fbd42ed80a5ddabf6dc96566afd99c189
siminos <siminos@zero.physics.gatech.edu>
2006-07-06 21:03:16 +0000

    Moved data into a seperate directory. Created interface for the program to accept the path of the data directory as a mandatory command line argument.

--- programs/testing/rpoNewt_ks/trunk/main.f90
+++ programs/testing/rpoNewt_ks/trunk/main.f90
@@ -19,9 +19,19 @@ integer(i8b) :: invplan, plan ! needed by fftw3
 integer(i4b) :: d, k,i
 real(dp) :: T,kappa, ti,tf
 real(dp) :: tolbc,tolf,damp=13.0_dp
-integer(i4b) :: ntrial
+character*10 :: wd
+integer(i4b) :: nargs
 
-open(21,file='parameters.dat')
+nargs=iargc()
+
+if (nargs .ne. 1) then
+	print *,"Program must be called with exactly one argument indicating the data directory."
+	call exit
+end if
+
+call getarg(1,wd)
+
+open(21,file=trim(wd)//'/parameters.dat')
 	read(21,*)
 	read(21,*) d
 	read(21,*)
@@ -31,7 +41,7 @@ open(21,file='parameters.dat')
 	read(21,*)
 	read(21,*) tolf
 	read(21,*)
-	read(21,*) ntrial
+	read(21,*) Ntrial
 	read(21,*) 
 	read(21,*) Nsteps
 	read(21,*) 
@@ -52,13 +62,13 @@ allocate(aIn(d/2))
 
 aIn=0.0_dp
 
-open(19,file='rpoGuess.dat')
+open(19,file=trim(wd)//'/rpoGuess.dat')
  
 	read(19,*) v(1:d)
  
 close(19)
 
-open(20,file='periodsGuess.dat')
+open(20,file=trim(wd)//'/periodsGuess.dat')
  
 	read(20,*) T
 	read(20,*) kappa
@@ -74,11 +84,11 @@ a=a/size(v)
 bc(1:d/2)=real(a(2:size(a)))
 bc(d/2+1:d)= aimag(a(2:size(a)))
 
-call mnewtRPO(ntrial,bc,tolbc,tolf,T,kappa,ksFJ)
+call mnewtRPO(Ntrial,bc,tolbc,tolf,T,kappa,ksFJ)
 
 print *,"T",T,"kappa",kappa
 
-open(27,file='periods.dat')
+open(27,file=trim(wd)//'/periods.dat')
 	write (27,"(F20.15)") T
 	write (27,"(F20.15)") kappa
 close(27)
@@ -92,7 +102,7 @@ call dfftw_plan_dft_c2r_1d(invplan,d,a,v,FFTW_ESTIMATE)
 call dfftw_execute(invplan)
 call dfftw_destroy_plan(invplan)
 
-open(28,file='rpo.dat')
+open(28,file=trim(wd)//'/rpo.dat')
 	write(28,222) v
 close(28)
 
