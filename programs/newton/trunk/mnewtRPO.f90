eecec56d9c39c14abff81a4838b39b0fff432a9b
siminos <siminos@zero.physics.gatech.edu>
2006-07-08 23:57:59 +0000

    Declared auxiliary variables sumx, sumf.

--- programs/newton/trunk/mnewtRPO.f90
+++ programs/newton/trunk/mnewtRPO.f90
@@ -23,12 +23,14 @@
 	REAL(DP) :: ndum
 	REAL(DP), DIMENSION(size(x)+2) :: fvec,p
 	REAL(DP), DIMENSION(size(x)+2,size(x)+2) :: fjac
+	real(dp) :: sumx,sumf
 
 	do  i=1,ntrial
 		print *,"Newton iteration #", i
 		call usrfun(x,fvec,fjac,T,kappa)
-		if (sum(abs(fvec)) <= tolf) then
-			print *,"Condition for fvec met, with fvec=",sum(abs(fvec))
+		sumf=sum(abs(fvec))
+		if ( sumf <= tolf) then
+			print *,"Condition for fvec met, with fvec=",sumf
 			newton_condition_met=1
 			RETURN
 		endif
@@ -37,10 +39,10 @@
 		x=x+p(1:size(p)-2)
 		T=T+p(size(p)-1)
 		kappa=kappa+p(size(p))
-		print *,"DT",p(size(p)-1),p(size(p))
-		print *,"Delta p",sum(abs(p))
+		sumx=sum(abs(p))
+		print *,"Delta x",sumx
 		if (sum(abs(p)) <= tolx) then 
-			print *,"Condition for tolx met, with tolx=",sum(abs(p))
+			print *,"Condition for tolx met, with tolx=",sumx
 			newton_condition_met=2
 			RETURN
 		end if
