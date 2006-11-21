e5ac39b510520558d47fa77f3408be90ac260e09
siminos <siminos@zero.physics.gatech.edu>
2006-11-21 18:35:47 +0000

    Prepare folders and files for addition of relative equilibria through continuation from L=12. Added customized grep script.

--- programs/libraries/ks/trunk/ks.f90
+++ programs/libraries/ks/trunk/ks.f90
@@ -5,12 +5,12 @@ use nrtype
 real(dp), dimension(:,:), allocatable :: Jac
 real(dp), dimension(:), allocatable :: lin,f0,f1,f2,f3,e,e2
 real(dp), dimension(:), allocatable :: f0dum,f1dum,f2dum,f3dum,edum,e2dum
-real(dp) :: R, L
+real(dp) :: R, L, Tw
 integer(i4b) :: d, M, Nsteps, Nplt, Ntrial
 real(dp) :: tolbc, tolf
 
 interface ksFJ
-	module procedure ksFJ_equil,ksFJ_rpo!(bc,fvec,fjac)
+	module procedure ksFJ_equil,ksFJ_rpo, ksFJ_req!(bc,fvec,fjac)
 ! 		use nrtype
 ! 		implicit none
 ! 		real(dp), DIMENSION(:), INTENT(IN) :: bc
@@ -301,33 +301,144 @@ call etdrk4DiagPrefactors(lin,h2,R,M,f0dum,f1dum,f2dum,f3dum,edum,e2dum)
 
 call etdrk4DiagJDriverSh(ti,ai,Jac,h,Nsteps,tf,af,Jac,f0,f1,f2,f3,e,e2,f0dum,f1dum,f2dum,f3dum,edum,e2dum,Nplt,etdrk4diag,etdrk4DiagJhr,SetNlin_KS,SetANdiag_KS)
 
-! open(17,file='a.dat')
-! open(21,file='v.dat')
-! open(27,file='aI.dat')
-! open(28,file='aR.dat')
-! 	do k=1,size(aSt,1)	
-! 		write(17,223) real(aSt(k,3)),aimag(aSt(k,3)),real(aSt(k,5)),tSt(k)
-! 		write(27,222) aimag(aSt(k,:))
-! 		write(28,222) real(aSt(k,:))
-! 		adum=aSt(k,:)
-! 		call dfftw_plan_dft_c2r_1d(invplan,d,adum,v,FFTW_ESTIMATE)
-! 		call dfftw_execute(invplan)
-! 		call dfftw_destroy_plan(invplan)
-! 		write(21,221) v
-! 	end do
-! !	write(17,'(4F15.10)') aimag(af(1))*L,aimag(af(2))*L,aimag(af(5))*L,tf/(L**2)	
-! close(17)
-! close(21)
-! close(27)
-! close(28)
+R_c=Rc(-kappa/L,size(af))
 
-! open(35,file='J.dat')
-! 	do i=1,d
-! 		write(35,221) Jacf(i,:) 
-! 	end do
-! close(35)
+R_r=Rr(kappa/L,d)
 
-!print *,"ai-af",ai-af
+Raf=R_c*af
+
+fvec(1:d/2)=real(ai(2:size(ai))-Raf(2:size(ai)))
+fvec(d/2+1:d)=aimag(ai(2:size(ai))-Raf(2:size(ai)))
+fvec(d+1)=0.0_dp
+fvec(d+2)=0.0_dp
+
+!print *,"af",sum(abs(af)),sum(abs(Raf))
+!print *,"fvec all", fvec
+
+!!! Compute v at af for the LHS
+
+call SetNlin_KS(af,N_adum)
+
+do k=1,d/2+1
+        v_c(k) = lin(k)*af(k) + N_adum(k)
+end do
+
+v_c=R_c*v_c
+
+v(1:d/2)=real(v_c(2:d/2+1))
+v(d/2+1:d)=aimag(v_c(2:d/2+1))
+
+! !!! Compute v at R_c*af for comparison
+! 
+! call SetNlin_KS(Raf,N_adum)
+! 
+! do k=1,d/2+1
+!         v_c_dum(k) = lin(k)*Raf(k) + N_adum(k)
+! end do
+! 
+! print *,"compare", sum(abs(v_c_dum-v_c))
+
+!!! Compute vi at ai for the LHS
+
+call SetNlin_KS(ai,N_adum)
+
+do k=1,d/2+1
+        v_c(k) = lin(k)*ai(k) + N_adum(k)
+end do
+
+vi(1:d/2)=real(v_c(2:d/2+1))
+vi(d/2+1:d)=aimag(v_c(2:d/2+1))
+
+do k=1,d/2+1
+	DR(k)=ii*Real((k-1),dp)/L
+end do
+
+DRRa= DR*(Raf)
+
+fjac=0.0_dp
+
+fjac(1:d,1:d)=UnitMatrix(d)-matmul(R_r,Jac)
+fjac(1:d,d+1)=-v
+fjac(1:d/2,d+2)=real(DRRa(2:d/2+1))
+fjac(d/2+1:d,d+2)=aimag(DRRa(2:d/2+1))
+fjac(d+2,1:d/2)=real(DR(2:d/2+1)*ai(2:d/2+1))
+fjac(d+2,d/2+1:d)=aimag(DR(2:d/2+1)*ai(2:d/2+1))
+fjac(d+1,1:d)= vi
+
+print *,"fvec", sum(abs(fvec))
+
+END SUBROUTINE ksFJ_rpo
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+SUBROUTINE ksFJ_req(bc,fvec,fjac,kappa)
+USE nrtype
+use nrutil, only:assert_eq
+use ifc_integr
+use ifc_util
+use f95_lapack, only: LA_GEES
+USE LA_PRECISION, ONLY: WP => DP
+!use ifc_ks
+
+IMPLICIT NONE
+
+include "fftw3.f"
+
+real(dp), DIMENSION(:), INTENT(IN) :: bc
+real(dp), DIMENSION(:), INTENT(OUT) :: fvec
+real(dp), DIMENSION(:,:), INTENT(OUT) :: fjac
+real(dp), intent(in) :: kappa
+!!!!
+complex(dpc), dimension(size(bc)/2+1) :: ai,af,adum,Rai,Raf, R_cadum
+complex(dpc), dimension(size(bc)/2+1) :: N_adum
+complex(dpc), dimension(size(bc)/2+1) :: v_c,v_c_dum
+real(dp), dimension(size(bc)) :: v,vi
+! real(dp), dimension(size(bc),size(bc)):: Jdum
+integer(i4b):: k,i,ndum
+real(dp) :: ti,tf,h,h2
+! real(dp), dimension(size(fjac,1)):: wR,wI
+integer(i8b) :: invplan, plan ! needed by fftw3
+complex(dpc), dimension(size(bc)/2+1) :: R_c, DR, DRRa
+real(dp), dimension(size(bc),size(bc)) :: R_r,Ri_r
+
+221 Format(<d>F21.16)
+222 Format(<d/2+1>F21.16)
+223 Format(<4>F21.16)
+
+ndum=assert_eq(size(bc),size(fvec)-2,'ksFJ1')
+ndum=assert_eq(d,size(fjac,1)-2,size(fjac,2)-2,'ksFJ2')
+
+if ( .not. allocated(Jac)) allocate(Jac(d,d))
+if ( .not. allocated(lin)) allocate(lin(d/2+1))
+
+fvec=0.0_dp
+ti=0.0_dp
+tf=Tw
+
+print *,"T",Tw
+print *,"kappa",kappa
+
+ai=(0.0_dp,0.0_dp)
+ai(2:size(ai))= bc(1:d/2)+ ii*bc(d/2+1:d)
+af=(0.0_dp,0.0_dp)
+
+print *,"ai", sum(abs(ai))
+
+Jac=UnitMatrix(d)
+
+h=abs(tf-ti)/Nsteps
+
+h2=h/2.0_dp
+
+call SetLin_KS(lin)
+call etdrk4DiagPrefactors(lin,h,R,M,f0,f1,f2,f3,e,e2)
+call etdrk4DiagPrefactors(lin,h2,R,M,f0dum,f1dum,f2dum,f3dum,edum,e2dum)
+
+
+call etdrk4DiagJDriverSh(ti,ai,Jac,h,Nsteps,tf,af,Jac,f0,f1,f2,f3,e,e2,f0dum,f1dum,f2dum,f3dum,edum,e2dum,Nplt,etdrk4diag,etdrk4DiagJhr,SetNlin_KS,SetANdiag_KS)
 
 R_c=Rc(-kappa/L,size(af))
 
@@ -393,17 +504,9 @@ fjac(d+2,1:d/2)=real(DR(2:d/2+1)*ai(2:d/2+1))
 fjac(d+2,d/2+1:d)=aimag(DR(2:d/2+1)*ai(2:d/2+1))
 fjac(d+1,1:d)= vi
 
-! Jdum=matmul(R_r,Jac)
-! ! Jdum=Jf
-
-
-! wR=0.0_dp
-! wI=0.0_dp
-! call la_gees(Jdum,wR(1:d),wI(1:d))
-! print *,"eig", wR(1:d/8)+ii*wI(1:d/8)
 print *,"fvec", sum(abs(fvec))
 
-END SUBROUTINE ksFJ_rpo
+END SUBROUTINE ksFJ_req
 
 
 end module
---
+++ programs/production/KS22.0/req/main.f90
@@ -0,0 +1,171 @@
+program shootME
+
+use nrtype
+use ifc_newt
+use ifc_integr
+use f95_lapack, only: LA_GEESX
+use la_precision, only: wp => dp
+use ifc_util
+use ks
+
+implicit none
+
+include "fftw3.f"
+
+real(dp), dimension(:), allocatable :: v,vdum, bc
+real(dp), dimension(:), allocatable :: wR,wI
+complex(dpc), dimension(:),allocatable :: a,adum
+complex(dpc), dimension(:), allocatable :: ai,af
+integer(i8b) :: invplan, plan ! needed by fftw3
+integer(i4b) :: k,i, sdim, Nrep=3
+real(dp) :: T,kappa, ti,tf, h, h2
+real(dp) :: damp=13.0_dp
+character*64 :: wd
+integer(i4b) :: nargs
+logical :: logicdum
+
+nargs=iargc()
+
+if (nargs .ne. 1) then
+	print *,"Program must be called with exactly one argument indicating the data directory."
+	call exit
+end if
+
+call getarg(1,wd)
+
+open(21,file=trim(wd)//'/parameters.dat')
+	read(21,*)
+	read(21,*) d
+	read(21,*)
+	read(21,*) L 
+	read(21,*)
+	read(21,*) tolbc
+	read(21,*)
+	read(21,*) tolf
+	read(21,*)
+	read(21,*) Ntrial
+	read(21,*) 
+	read(21,*) Nsteps
+	read(21,*) 
+	read(21,*) Nplt
+	read(21,*)
+	read(21,*) M
+	read(21,*)
+	read(21,*) R
+close(21)
+
+220 Format(F30.18)
+221 Format(<d>F30.18)
+222 Format(<d/2+1>F30.18)
+223 Format(<4>F30.18)
+
+allocate(v(d),vdum(d),bc(d))
+allocate(a(d/2+1),adum(d/2+1),ai(d/2+1),af(d/2+1))
+allocate(lin(d/2+1),f0(d/2+1),f1(d/2+1),f2(d/2+1),f3(d/2+1),e(d/2+1),e2(d/2+1))
+allocate(f0dum(d/2+1),f1dum(d/2+1),f2dum(d/2+1),f3dum(d/2+1),edum(d/2+1),e2dum(d/2+1))
+allocate(wR(d),wI(d))
+
+open(19,file=trim(wd)//'/rpoGuess.dat')
+ 
+	read(19,*) v(1:d)
+ 
+close(19)
+
+open(20,file=trim(wd)//'/periodsGuess.dat')
+ 
+	read(20,*) T
+	read(20,*) kappa
+ 
+close(20)
+
+call dfftw_plan_dft_r2c_1d(plan,d,v,a,FFTW_ESTIMATE)
+call dfftw_execute(plan)
+call dfftw_destroy_plan(plan)
+a=a/size(v)
+
+bc(1:d/2)=real(a(2:size(a)))
+bc(d/2+1:d)= aimag(a(2:size(a)))
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+call mnewtRPO(Ntrial,bc,tolbc,tolf,T,kappa,ksFJ_rpo)
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+print *,"T",T,"kappa",kappa
+
+open(27,file=trim(wd)//'/periods.dat')
+	write (27,220) T
+	write (27,220) kappa
+close(27)
+
+if ( newton_condition_met .ne. 0) then
+	if ( newton_condition_met .eq. 2) then
+		ti=0.0_dp
+		tf=T
+		h=abs(tf-ti)/Nsteps
+		h2=h/2.0_dp
+		call SetLin_KS(lin)
+		call etdrk4DiagPrefactors(lin,h,R,M,f0,f1,f2,f3,e,e2)
+		call etdrk4DiagPrefactors(lin,h2,R,M,f0dum,f1dum,f2dum,f3dum,edum,e2dum)
+		ti=0.0_dp
+		ai=(0.0_dp,0.0_dp)
+		ai(2:size(a))=bc(1:size(bc)/2)+ii*bc(size(bc)/2+1:size(bc))
+		af=(0.0_dp,0.0_dp)
+		Jac=UnitMatrix(d)
+		call etdrk4DiagJDriverSh(ti,ai,Jac,h,Nsteps,tf,af,Jac,f0,f1,f2,f3,e,e2,f0dum,f1dum,f2dum,f3dum,edum,e2dum,Nplt,etdrk4diag,etdrk4DiagJhr,SetNlin_KS,SetANdiag_KS)
+	end if
+	wR=0.0_dp
+	wI=0.0_dp
+	Jac=matmul(Rr(kappa/L,d),Jac)
+	call la_geesx(Jac,wR(1:d),wI(1:d),select=SelectLargeEig_r,sdim=sdim)
+	print *,"eig", wR(1:sdim)+ii*wI(1:sdim)
+	open(35,file=trim(wd)//'/Jdiag.dat')
+	do i=1,sdim
+		write(35,"(2F30.18)") wR(i)+ii*wI(i)
+	enddo
+	close(35)
+else
+	stop "Newton Condition didn't met."
+endif
+
+ti=0.0_dp
+ai=(0.0_dp,0.0_dp)
+ai(2:size(a))=bc(1:size(bc)/2)+ii*bc(size(bc)/2+1:size(bc))
+print *,sum(abs(ai))
+af=(0.0_dp,0.0_dp)
+tf=real(Nrep,dp)*T
+h=abs(tf-ti)/(Nrep*Nsteps)
+h2=h/2.0_dp
+
+open (33,file=trim(wd)//'/timestep.dat')
+write (33,220) h2
+close(33)
+
+call SetLin_KS(lin)
+call etdrk4DiagPrefactors(lin,h2,R,M,f0,f1,f2,f3,e,e2)
+call etdrk4DiagDriverS(ti,ai,2*Nrep*Nsteps,tf,af,f0,f1,f2,f3,e,e2,Nplt,SetNlin_KS)
+open (29,file=trim(wd)//'/rpoU.dat')
+do i=1,size(aSt,1)
+	adum=aSt(i,:)
+!	print *,sum(abs(adum))
+	call dfftw_plan_dft_c2r_1d(invplan,d,adum,v,FFTW_ESTIMATE)
+	call dfftw_execute(invplan)
+	call dfftw_destroy_plan(invplan)
+	write(29,221) v		
+end do
+close(29)
+
+a=(0.0_dp,0.0_dp)
+a(2:size(a))=bc(1:size(bc)/2)+ii*bc(size(bc)/2+1:size(bc))
+
+adum=a
+
+call dfftw_plan_dft_c2r_1d(invplan,d,adum,v,FFTW_ESTIMATE)
+call dfftw_execute(invplan)
+call dfftw_destroy_plan(invplan)
+
+open(28,file=trim(wd)//'/rpoUic.dat')
+	write(28,221) v
+close(28)
+
+
+end program
