35f70230403db4652e7d44253be6358005426a7f
siminos <siminos@zero.physics.gatech.edu>
2006-11-24 18:21:31 +0000

    Added data files for w-1L relative equilibrium. Hopefully fixed bugs in shooting code (wrong interface - wrong dimensions of arrays.

--- programs/libraries/newton/trunk/mnewtTW.f90
+++ programs/libraries/newton/trunk/mnewtTW.f90
@@ -1,4 +1,4 @@
-	SUBROUTINE mnewtTW(ntrial,x,tolx,tolf,T,kappa,usrfun)
+	SUBROUTINE mnewtTW(ntrial,x,tolx,tolf,kappa,usrfun)
 	USE nrtype
 	use la_precision, only: wp => dp
 	use f95_lapack, only: LA_GESV
@@ -7,7 +7,6 @@
 	INTEGER(I4B), INTENT(IN) :: ntrial
 	REAL(DP), INTENT(IN) :: tolx,tolf
 	REAL(DP), DIMENSION(:), INTENT(INOUT) :: x
-	real(dp), intent(in) :: T
 	real(dp), intent(inout) :: kappa
 	INTERFACE
 		SUBROUTINE usrfun(x,fvec,fjac,kappa)
@@ -21,8 +20,8 @@
 	END INTERFACE
 	INTEGER(I4B) :: i
 	REAL(DP) :: ndum
-	REAL(DP), DIMENSION(size(x)+1) :: fvec,p
-	REAL(DP), DIMENSION(size(x)+1,size(x)+1) :: fjac
+	REAL(DP), DIMENSION(size(x)+2) :: fvec,p
+	REAL(DP), DIMENSION(size(x)+2,size(x)+2) :: fjac
 
 	do  i=1,ntrial
 		print *,"Newton iteration #", i
@@ -32,7 +31,7 @@
 		endif
 		p=-fvec
 		call la_gesv(fjac,p)
-		x=x+p(1:size(p)-1)
+		x=x+p(1:size(p)-2)
 		kappa=kappa+p(size(p))
 		print *,"Dkappa",p(size(p))
 		if (sum(abs(p)) <= tolx) then 
