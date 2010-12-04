6b0b4b28c3dac77fbab9e1f81a6815162cae15d8
siminos <siminos@zero.physics.gatech.edu>
2010-12-04 16:43:10 +0000

    Sprucing up homepage, ignore.

--- www/src/index.php
+++ www/src/index.php
@@ -6,6 +6,7 @@
   <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-5">
 
   <link rel="stylesheet" type="text/css" href="css/homepage.css"><title>Evangelos Siminos: Center for Nonlinear Science ; Georgia Tech</title></head><body>
+  <link rel="stylesheet" type="text/css" href="css/local.css">
 
 <table bgcolor="#ffffff" border="0" width="740">
 
@@ -56,9 +57,65 @@
 
       <h4 class="title2"> <a name="research"></a>Research</h4>
 
-      <p class="main"> My research interests include:</p>
+<!--       <h5 class="title3"> <a name="vlasov"></a>Stability of Vlasov-Poisson equilibria.</h5> -->
 
-      <ul>
+      <p class="research"> My research interests lie in the area 
+	of nonlinear dynamics of spatially extended systems. I focus on the application
+	of methods of dynamical systems theory and the development of
+	new computational techniques suitable for high-dimensional
+	systems. Recent research projects include:
+
+      <p class="research"> <a name="vlasov"></a><span class="research-title">Stability of Vlasov-Poisson equilibria.</span>
+	The Vlasov-Poisson system describes electrostatic phenomena in collisionless plasmas.
+	The accurate and efficient computation of linear stability properties of nonlinear 
+	steady states within this framework is important both for theory and applications, for
+	instance in the description of nonlinear saturation of processes such as 
+	stimulated Raman scattering.
+	However, the presence of a continuum spectrum of neutral eigenmodes, associated
+	to the transfer of energy to finer velocity scales, makes the
+	problem hard to tackle.
+	We have recently developed a general and efficient method to compute 
+	the dominant unstable modes of nonlinear steady states of the Vlasov-Poisson system, 
+	through a combination of spectral deformation (to "damp" the neutral spectrum) 
+	and Galerkin spectral methods (to reduce the problem to a sparse matrix eigenproblem). 
+	<a href="papers/sbg11.pdf" class="checkers">Read more ...</a>
+      </p>
+      <p class="research"> <a name="reduction"></a><span class="research-title">Dynamics of spatially extended systems with continuous symmetries.</span>
+	In dynamical systems studies of turbulence the organization of the infinite-dimensional state space 
+	can be understood in terms of equilibria, periodic orbits and their stable and unstable manifolds, 
+	which serve to connect local neighborhoods. However, when traveling wave solutions are taken 
+	into account, the geometric picture is obscured by the presence of equivalent, 
+	up to a continuous symmetry transformation, trajectories. Continuous symmetry
+	reduction (i.e. the identification of symmetry related trajectories) is required 
+	before the dynamical systems approach can then be successfully applied.
+	Read more on: (a) <a href="papers/CDS10.pdf" class="checkers">The state space geometry of the Kuramoto-Sivashinsky system</a>, 
+	a one-dimensional "toy-model" for turbulence, 
+	(b) <a href="papers/SiCvi10.pdf" class="checkers">Continuous symmetry reduction</a> in high-dimensional flows,
+	(c) How symmetry reduction might be applied in the Kuramoto-Sivashinsky system (<a href="http://www.chaosbook.org/projects/Siminos/thesis.pdf" class="checkers">my thesis</a>, 6.5MB).
+      </p>
+      <p class="research"> <a name="solitons"></a><span class="research-title">Relativistic solitary waves in plasmas as connecting orbits.</span>
+	The current trend in the field of laser-plasma interaction, is to use short, 
+	high-intensity laser pulses to accelerate particles
+	in much higher energies than possible with conventional accelerators 
+	of comparable size. Short laser pulses are hard to produce and control and
+	an open question is whether longer pulses could be utilized for
+	the same purpose of particle acceleration. A first step in this direction
+	is to connect the problem to the existence of solitary waves 
+	in a fluid-plasma electromagnetic model. 
+	In turn, this problem can be reduced to that of finding 
+	homoclinic and heteroclinic connections
+	of a Hamiltonian system of ordinary differential equations. Then,
+	dynamical systems theory allows a systematic determination and classification
+	of solitary wave solutions. We are currently working on the problem of
+	excitation of such solutions through laser-matter interaction
+	and their stability in a kinetic framework, in order to determine 
+	whether the solitary waves could transfer   
+	energy to the particles through wavebreaking or parametric instabilities. <a href="papers/SSL10.pdf" class="checkers">Read more ...</a>
+
+      </p>
+
+
+<!--      <ul>
 
         <li>spatially extended systems&nbsp;</li>
 
@@ -66,7 +123,7 @@
 
 	<li>nonlinear dynamics of plasmas</li>
 
-      </ul>
+      </ul>-->
 
 <!--      <p class="main"> My recent research focuses in the
 dynamics of Kuramoto-Sivashinsky equation.</p>-->
@@ -88,6 +145,8 @@ dynamics of Kuramoto-Sivashinsky equation.</p>-->
 
       </div>
 
+      <p class="posted">last updated Nov. 14 2010</p>
+
       </div>
 
       </td>
@@ -97,7 +156,6 @@ dynamics of Kuramoto-Sivashinsky equation.</p>-->
   </tbody>
 </table>
 
-<p class="posted">last updated Nov. 14 2010</p>
 
 <?php include("include/counter.js");  ?>
 
