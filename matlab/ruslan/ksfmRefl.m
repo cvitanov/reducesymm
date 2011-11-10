541cf3cbd9b64bfe3e4de4be9374b713626b31ee
siminos <siminos@zero.physics.gatech.edu>
2011-11-10 13:51:27 +0000

    Ignore.

--- matlab/ruslan/ksfmRefl.m
+++
@@ -1,8 +0,0 @@
-function [aa] = ksfmRefl(a)
-% Apply reflection operator to KS point in Fourier modes representation.
-
-aa=a;
-aa(1:2:end-1)=-a(1:2:end-1);
-
-end
-
--- matlab/ruslan/shiftrpo.m
+++
@@ -1,9 +0,0 @@
-function [ts, as] = shiftrpo(tt, aa, L, tend, ph, ishift)
-%  Shift initial point of RPO to tt(ishift).
-  if ishift > 1 & ishift <= length(tt),
-    ts = [tt(ishift:end) tt(2:ishift)+tend]-tt(ishift);
-    aas = aa(:,2:ishift);
-    vs = (aas(1:2:end,:)+1i*aas(2:2:end,:));
-    vs = vs.*repmat(exp((2i*pi/L).*ph.*(1:size(vs,1))'),1,ishift-1);
-    aas(1:2:end,:) = real(vs);  aas(2:2:end,:) = imag(vs);
-    as = [aa(:,ishift:end) aas]; end
