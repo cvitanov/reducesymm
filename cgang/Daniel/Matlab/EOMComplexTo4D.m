08d2a22a31f4509c20310bd162d1ebcb5d86e5b5
dborrero <dborrero@zero.physics.gatech.edu>
2012-04-29 22:11:05 +0000

    IMPORTANT: Updated notation in 2mode "paper", blog, and MY matlab codes from $z_1 = x_1 + i y_1$ and $z_2=x_2 + i y_2$ to $z_1 = x_1 + i x_2$ and $z_2 = y_1+ i y_2$ to reflect notation used in analysis of Complex Lorenz in DasBuch as per Predrag's request. Although we've sort of moved on to $(u,v,w,q)$, I suggest that everybody take notice and start using this notation if you need to use 4D cartesian coordinates for anything. Read notice in blog for more info on fixing your codes, etc...

--- cgang/Daniel/Matlab/EOMComplexTo4D.m
+++ cgang/Daniel/Matlab/EOMComplexTo4D.m
@@ -8,17 +8,17 @@
 clear all
 
 % Intialize symbolic variables
-syms x1 x2 y1 y2 a1 a2 b1 b2 c1 c2 mu1 mu2 e2 real
+syms x1 y1 x2 y2 a1 a2 b1 b2 c1 c2 mu1 mu2 e2 real
 syms r1dot theta1 theta1dot r2dot theta2 theta2dot real
 syms r1 r2 positive
-syms Psi real
+syms phi real
 
 % Express complex variables z in terms of real and imaginary parts
 % Notice that 1's form one complex number and 2's form another, whereas
 % in the Complex Lorenz example of Chaosbook, x's form one complex number
 % and y's form another.
-z1 = x1 + i*y1;
-z2 = x2 + i*y2;
+z1 = x1 + i*x2;
+z2 = y1 + i*y2;
 
 % Define zdot's as per Siminos's eq:AGpolar in Atlas blog.
 z1dot = mu1*z1 + a1*z1*z1*conj(z1) + b1*z1*z2*conj(z2) + c1*conj(z1)*z2;
@@ -27,34 +27,34 @@ z2dot = (mu2 -i*e2)*z2 + a2*z2*z1*conj(z1) + b2*z2*z2*conj(z2) + c2*z1*z1;
 % Calculate 4D flow by taking real and imaginary parts of zdot equations
 disp('The 2-mode system expressed in terms of 4 real variables')
 x1dot = simplify(real(z1dot))
-y1dot = simplify(imag(z1dot))
-x2dot = simplify(real(z2dot))
+x2dot = simplify(imag(z1dot))
+y1dot = simplify(real(z2dot))
 y2dot = simplify(imag(z2dot))
 
 % Solve for equations in polar coordinates 
-% {x1,y1,x2,y2} -> {r1*cos(theta1),r1*sin(theta1),r2*cos(theta2),r2*sin(theta2)}
-eq1 = subs(x1dot,{x1,y1,x2,y2},{r1*cos(theta1),r1*sin(theta1),r2*cos(theta2),r2*sin(theta2)})...
+% {x1,x2,y1,y2} -> {r1*cos(theta1),r1*sin(theta1),r2*cos(theta2),r2*sin(theta2)}
+eq1 = subs(x1dot,{x1,x2,y1,y2},{r1*cos(theta1),r1*sin(theta1),r2*cos(theta2),r2*sin(theta2)})...
       -r1dot*cos(theta1) + r1*sin(theta1)*theta1dot;
-eq2 = subs(y1dot,{x1,y1,x2,y2},{r1*cos(theta1),r1*sin(theta1),r2*cos(theta2),r2*sin(theta2)})...
+eq2 = subs(x2dot,{x1,x2,y1,y2},{r1*cos(theta1),r1*sin(theta1),r2*cos(theta2),r2*sin(theta2)})...
       -r1dot*sin(theta1) - r1*cos(theta1)*theta1dot;
-eq3 = subs(x2dot,{x1,y1,x2,y2},{r1*cos(theta1),r1*sin(theta1),r2*cos(theta2),r2*sin(theta2)})...
+eq3 = subs(y1dot,{x1,x2,y1,y2},{r1*cos(theta1),r1*sin(theta1),r2*cos(theta2),r2*sin(theta2)})...
       -r2dot*cos(theta2) + r2*sin(theta2)*theta2dot;
-eq4 = subs(y2dot,{x1,y1,x2,y2},{r1*cos(theta1),r1*sin(theta1),r2*cos(theta2),r2*sin(theta2)})...
+eq4 = subs(y2dot,{x1,x2,y1,y2},{r1*cos(theta1),r1*sin(theta1),r2*cos(theta2),r2*sin(theta2)})...
       -r2dot*sin(theta2) - r2*cos(theta2)*theta2dot;
 
 S = solve(eq1,eq2,eq3,eq4,'r1dot','r2dot','theta1dot','theta2dot');
 
 disp('Substituting {r1*cos(theta1),r1*sin(theta1),r2*cos(theta2),r2*sin(theta2)}')
-disp('for {x1,y1,x2,y2} and solving for the velocities in these coordinates, we get:')
+disp('for {x1,x2,y1,y2} and solving for the velocities in these coordinates, we get:')
 r1dot = simple(S.r1dot)
 theta1dot = simple(S.theta1dot)
 r2dot = simple(S.r2dot)
 theta2dot = simple(S.theta2dot)
 
 disp('Notice that theta1 and theta2 only appear in the combination (2*theta1 - theta2)')
-disp('Recasting the equations in terms of Psi = 2*theta1 - theta2,')
+disp('Recasting the equations in terms of phi = 2*theta1 - theta2,')
 disp('we can reduce the problem to a 3D system')
-r1dot = subs(r1dot,2*theta1-theta2,Psi)
-r2dot = subs(r2dot,2*theta1-theta2,Psi)
-psidot = subs(2*theta1dot - theta2dot,2*theta1-theta2,Psi)
+r1dot = simplify(subs(r1dot,2*theta1-theta2,phi))
+r2dot = simplify(subs(r2dot,2*theta1-theta2,phi))
+phidot = simplify(subs(2*theta1dot - theta2dot,2*theta1-theta2,phi))
 
--- cgang/Daniel/Matlab/PKPointStability.m
+++ cgang/Daniel/Matlab/PKPointStability.m
@@ -3,22 +3,22 @@
 % Calculates the eigenvalues and eigenvectors of 
 % the stability matrix for the Porter-Knobloch system in
 % for a generic point given in the 4D Cartesian representation
-% (X1,Y1,X2,Y2) for a given array of parameters
+% (X1,x2,y1,Y2) for a given array of parameters
 % params = {a1,a2,b1,b2,c1,c2,mu1,mu2,2}.
 
-function [E,Lambda] = PKPointStability(X1,Y1,X2,Y2,params)
+function [E,Lambda] = PKPointStability(X1,x2,y1,Y2,params)
 % Intialize symbolic variables
-syms x1 x2 y1 y2 a1 a2 b1 b2 c1 c2 mu1 mu2 e2 real
+syms x1 y1 x2 y2 a1 a2 b1 b2 c1 c2 mu1 mu2 e2 real
 
 % Specify 2-mode flow in Cartesian coordinates as solved for by EOMComplexTo4D.m
-x1dot = a1*x1^3 + b1*x1*x2^2 + c1*x1*x2 + a1*x1*y1^2 + b1*x1*y2^2 + mu1*x1 + c1*y1*y2;
-y1dot = a1*x1^2*y1 + c1*x1*y2 + b1*x2^2*y1 - c1*x2*y1 + a1*y1^3 + b1*y1*y2^2 + mu1*y1;
-x2dot = a2*x1^2*x2 + c2*x1^2 + b2*x2^3 + a2*x2*y1^2 + b2*x2*y2^2 + mu2*x2 - c2*y1^2 + e2*y2;
-y2dot = a2*x1^2*y2 + 2*c2*x1*y1 + b2*x2^2*y2 - e2*x2 + a2*y1^2*y2 + b2*y2^3 + mu2*y2;
+x1dot = a1*x1^3 + b1*x1*y1^2 + c1*x1*y1 + a1*x1*x2^2 + b1*x1*y2^2 + mu1*x1 + c1*x2*y2;
+x2dot = a1*x1^2*x2 + c1*x1*y2 + b1*y1^2*x2 - c1*y1*x2 + a1*x2^3 + b1*x2*y2^2 + mu1*x2;
+y1dot = a2*x1^2*y1 + c2*x1^2 + b2*y1^3 + a2*y1*x2^2 + b2*y1*y2^2 + mu2*y1 - c2*x2^2 + e2*y2;
+y2dot = a2*x1^2*y2 + 2*c2*x1*x2 + b2*y1^2*y2 - e2*y1 + a2*x2^2*y2 + b2*y2^3 + mu2*y2;
 
 % Define vectors of coordinates and time derivatives
-X = [x1 y1 x2 y2]';
-V = [x1dot y1dot x2dot y2dot]';
+X = [x1 x2 y1 y2]';
+V = [x1dot x2dot y1dot y2dot]';
 
 % Calculate the stability matrix
 A = jacobian(V,X);
@@ -29,8 +29,8 @@ C1 = params{5}; C2 = params{6}; MU1 = params{7}; MU2 = params{8};
 E2 = params{9};
 
 % Evaluate stability matrix at point
-A = subs(A,{x1,y1,x2,y2,a1,a2,b1,b2,c1,c2,mu1,mu2,e2},...
-    {X1,Y1,X2,Y2,A1,A2,B1,B2,C1,C2,MU1,MU2,E2});
+A = subs(A,{x1,x2,y1,y2,a1,a2,b1,b2,c1,c2,mu1,mu2,e2},...
+    {X1,x2,y1,Y2,A1,A2,B1,B2,C1,C2,MU1,MU2,E2});
 
 % Calculate eigenvectors E and eigenvalues Lambda
 [E,Lambda] = eig(A)
