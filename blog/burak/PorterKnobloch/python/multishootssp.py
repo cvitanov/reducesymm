Corrected the missing term in the multishoot formula, found some relative periodic orbits

blog/burak/PorterKnobloch/python/multishootssp.py
@@ -98,7 +98,7 @@ def Jacobian(x, Ti):
 
 	return J		
 
-tol = 1e-7
+tol = 1e-9
 earray = np.array([], float)
 xrpo = np.zeros(np.shape(xrpo0))
 tofrpo = np.zeros(np.shape(tof0))
@@ -106,7 +106,7 @@ phirpo = np.zeros(np.shape(phirpo0))
 
 Adaptive = True
 iAdaptiveMax = 10			
-factor = 10
+factor = 2
 			
 for i in range(1, int(np.max(group)+1)):
 
@@ -158,7 +158,7 @@ for i in range(1, int(np.max(group)+1)):
 	errorr = np.zeros(np.shape(error))
 	
 	#Define maximum number of iterations:
-	itermax = 300
+	itermax = 200
 	#Apply ChaosBook p294 (13.11) 
 	#with constraint nhat . Dx = 0
 	iteration=0
@@ -189,13 +189,14 @@ for i in range(1, int(np.max(group)+1)):
 			minusgvx = -np.dot(twomode.LieElement(phii[j]), vx)
 			minusgvx = minusgvx.reshape(-1,1)
 			
-			#minusTfTx term:
-			minusTfTx = -np.dot(T, xTj)
-			minusTfTx = minusTfTx.reshape(-1,1)
+			#minusTgfTx term:
+			minusTgfTx = np.dot(twomode.LieElement(phii[j]), xTj)
+			minusTgfTx = -np.dot(T, minusTgfTx)
+			minusTgfTx = minusTgfTx.reshape(-1,1)
 			
 			
 			Aj = np.concatenate((-np.dot(twomode.LieElement(phii[j]), J), minusgvx), axis=1)
-			Aj = np.concatenate((Aj, minusTfTx), axis=1)
+			Aj = np.concatenate((Aj, minusTgfTx), axis=1)
 			
 			#nhat0 = np.append(nhat.reshape(-1,1), np.array([[0], [0]], float))
 			
@@ -275,7 +276,7 @@ for i in range(1, int(np.max(group)+1)):
 				
 				else:					
 					
-					alpha = float(alpha) / factor
+					alpha = float(alpha) / float(factor)
 					
 				#raw_input("Press Enter to continue...")	
 
@@ -326,10 +327,10 @@ for i in range(1, int(np.max(group)+1)):
 	plot(np.arange(iteration), earray)
 	xlabel('Iteration')
 	ylabel('Error')
-	savefig('image/errorrposearch.png', bbox_inches='tight', dpi=150)
-	show()
+	savefig('image/errorrposearch'+str(i)+'.png', bbox_inches='tight', dpi=150)
+	#show()
 	
-	raw_input("Press Enter to continue...")
+	#raw_input("Press Enter to continue...")
 
 np.savetxt('data/tofrpo.dat',tofrpo)
 np.savetxt('data/xrpo.dat', xrpo)

blog/burak/PorterKnobloch/python/periodicorbits.py
@@ -71,12 +71,12 @@ for i in range(1,int(np.max(group))+1):
 	ax.set_xlabel('$x$', fontsize=18)
 	ax.set_ylabel('$y$', fontsize=18)
 	ax.set_zlabel('$z$', fontsize=18)
-	#ax.view_init(30,-110)
+	ax.view_init(25,35)
 	ax.grid(b='off')
 	
 	
 	fname = 'image/' + itinerary[0] + '.png'
 	savefig(fname, bbox_inches='tight', dpi=100)	
 
-	plt.show()	
-	raw_input("Press Enter to continue...")
+	#plt.show()	
+	#raw_input("Press Enter to continue...")

blog/burak/PorterKnobloch/python/psectandretmap.py
@@ -42,7 +42,7 @@ computesymbdyn = True
 plotpsandretmap = True
 solveFPO = False
 
-m = 2
+m = 6
 
 pars = np.loadtxt('data/parameters.dat')
 
@@ -156,11 +156,11 @@ snplus1sorted = snplus1[snsortedindices]
 print 'Interpolating to the return map'
 imax = np.argmax(snplus1sorted)
 tck1 = interpolate.splrep(snsorted[0:imax+1], snplus1sorted[0:imax+1])
-xint1 = np.linspace(snsorted[0], snsorted[imax], 1000)
+xint1 = np.linspace(snsorted[0], snsorted[imax], 50000)
 yint1 = interpolate.splev(xint1, tck1)
 
 tck2 = interpolate.splrep(snsorted[imax:len(snsorted)], snplus1sorted[imax:len(snsorted)])
-xint2 = np.linspace(snsorted[imax], snsorted[len(snsorted)-1], 1000)
+xint2 = np.linspace(snsorted[imax], snsorted[len(snsorted)-1], 50000)
 yint2 = interpolate.splev(xint2, tck2)
 
 snint = np.append(xint1, xint2)
@@ -198,7 +198,7 @@ for i in range(m):
 	#print "retmap %i" %(i+1)
 	
 	fpoevo=0
-	for s0 in np.arange(smin, smax, (smax-smin)/1000):
+	for s0 in np.arange(smin, smax, (smax-smin)/50000):
 		fpoev = fpo(s0)
 		
 		if fpoev * fpoevo < 0: #If there is a zero-crossing, look for the root:	
@@ -208,7 +208,7 @@ for i in range(m):
 			newcandidate = 1
 			for j in range(np.size(scandidates,0)):
 				#Discard if found candidate is previously found:
-				if np.abs(scandidates[j,1] - sc)<1e-12: 
+				if np.abs(scandidates[j,1] - sc)<1e-9: 
 					newcandidate=0
 				
 			if newcandidate: