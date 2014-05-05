7c1abd3fe4e35ced8d01369c0ff620934fc219b3
Tingnan Zhang <tingnan.zhang@gmail.com>
2014-05-05 15:52:54 -0400

    2014-05-05 15:52

--- tingnan/codes/zetafunc.m
+++ tingnan/codes/zetafunc.m
@@ -1,7 +1,6 @@
-function [ zetaval ] = zetafunc( lambdae, np)
+function [ zetaval ] = zetafunc(np, tp)
 
 zetaval = 1;
-tp = 1./lambdae;
 for ii = 1:np 
     tmpmat = combntns(tp, ii);
     tmpvec = prod(tmpmat, 2);
