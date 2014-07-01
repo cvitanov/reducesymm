7e36a4ddbb6ac572a8baaaa579293cdf81285b50
burak <burakbudanur@gmail.com>
2014-07-01 14:28:24 -0400

    added a little python script for Evangelos to read from the rpo database

---
+++ burak/2modes/ReadRpoDb.py
@@ -0,0 +1,45 @@
+import numpy as np
+import sqlite3
+from StringIO import StringIO 
+from subprocess import call
+from scipy.optimize import fsolve
+import sys
+import sympy
+#Initiate plotting environment:
+import matplotlib as mpl
+from pylab import plot, xlabel, ylabel, show, savefig
+import matplotlib.pyplot as plt
+import twomode
+
+FiniteGrammar=False
+
+if FiniteGrammar:
+    Ncycle = 79
+    NmaxExp = 12
+    conn = sqlite3.connect('data/rpo.db')
+
+else:
+    Ncycle = 127
+    NmaxExp = 12
+    conn = sqlite3.connect('data/rpoall.db')
+
+c = conn.cursor()
+rpos = []
+
+for rpono in range(1,Ncycle + 1):
+    c.execute("SELECT * FROM rpos WHERE rpono = "+str(rpono))
+    a = c.fetchall()
+    
+    rpono,  itinerary, x1, y1, x2, y2, T, phifetch , floquetFetch= a[0]
+    #Format the Floquet exponents such that it would be readable by loadtxt as
+    #a numpy array
+    floquetobj = StringIO(str(floquetFetch).strip('[ ]'))
+    floquet = np.loadtxt(floquetobj)
+    TopLength = len(str(itinerary))
+    
+    c.execute("SELECT phi FROM rpo"+str(itinerary))
+    a = c.fetchall()
+    phi = -sum([float(str(a[i]).strip('( ,)')) for i in range(len(a))])
+    rpos.append([TopLength, T, floquet[0], phi])
+
+conn.close()
